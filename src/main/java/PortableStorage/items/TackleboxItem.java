9a74e85e21176550a2c1ac53978b74af1073c545
GoliathX211 <30582936+GoliathX211@users.noreply.github.com>
2022-07-08 00:24:35 -0400

    Added the stupid fricken whatever

---
+++ src/main/java/PortableStorage/items/TackleboxItem.java
@@ -0,0 +1,42 @@
+package PortableStorage.items;
+
+import necesse.engine.localization.Localization;
+import necesse.entity.mobs.PlayerMob;
+import necesse.gfx.gameTooltips.ListGameTooltips;
+import necesse.inventory.InventoryItem;
+import necesse.inventory.item.Item;
+import necesse.inventory.item.miscItem.PouchItem;
+
+public class TackleboxItem extends PouchItem {
+    public TackleboxItem(Rarity rarity) {
+        this.rarity = rarity;
+        drawStoredItems =true;
+    }
+
+    @Override
+    public ListGameTooltips getTooltips(InventoryItem item, PlayerMob perspective) {
+        ListGameTooltips tooltips = super.getTooltips(item, perspective);
+        tooltips.add(Localization.translate("itemtooltip", "rclickinvopentip"));
+        tooltips.add(Localization.translate("itemtooltip", "storedItems", "items", this.getStoredItemAmounts(item)));
+        return tooltips;
+    }
+    @Override
+    public boolean isValidPouchItem(InventoryItem var1) {
+        return this.isValidRequestType(var1.item.type);
+    }
+
+    @Override
+    public boolean isValidRequestItem(Item var1) {
+       return this.isValidRequestType(var1.type);
+    }
+
+    @Override
+    public boolean isValidRequestType(Item.Type type) {
+        return type == Type.BAIT;
+    }
+
+    @Override
+    public int getInternalInventorySize() {
+        return 10;
+    }
+}
