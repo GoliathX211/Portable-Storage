4b451af295e94f211435a45b13302c14d3c99f35
GoliathX211 <30582936+GoliathX211@users.noreply.github.com>
2022-07-12 15:28:15 -0400

    Added Tacklebox to Angler's shop items as intended thanks to Kodi022's example code.

---
+++ src/main/java/PortableStorage/patches/getShopItemsPatch.java
@@ -0,0 +1,41 @@
+package PortableStorage.patches;
+
+import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.engine.network.server.ServerClient;
+import necesse.engine.util.GameRandom;
+import necesse.entity.mobs.friendly.human.humanShop.AnglerHumanMob;
+import necesse.level.maps.levelData.villageShops.ShopItem;
+import necesse.level.maps.levelData.villageShops.VillageShopsData;
+import net.bytebuddy.asm.Advice;
+
+import java.util.ArrayList;
+
+@ModMethodPatch(target = AnglerHumanMob.class, name = "getShopItems", arguments = {VillageShopsData.class, ServerClient.class})
+public class getShopItemsPatch {
+    @Advice.OnMethodExit()
+    static void onExit(
+            @Advice.This AnglerHumanMob angler,
+            @Advice.Return(readOnly = false) ArrayList<ShopItem> list
+    ) {
+        GameRandom random = GameRandom.globalRandom;
+        list.add(ShopItem.item(
+                "Tacklebox",
+                angler.getRandomHappinessPrice(
+                        random,
+                        1500,
+                        2000,
+                        1750)
+        ));
+    }
+}
+
+
+// Kodi022 Implementation
+// @ModMethodPatch(target = TravelingMerchantMob.class, name = "getShopItems", arguments = {VillageShopsData.class, ServerClient.class})
+// public class SilverTrade {
+//     @Advice.OnMethodExit()
+//     static void onExit(@Advice.This TravelingMerchantMob merch,
+//                         @Advice.Return(readOnly = false) ArrayList<ShopItem> list) {
+//         list.add(ShopItem.item("silveroreitem", 18));
+//     }
+// }
